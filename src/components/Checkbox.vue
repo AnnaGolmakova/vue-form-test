b3a7e9c42fb2dd4832c2288d45c206402252a779
Anna Golmakova <ann.golmakova@gmail.com>
2025-08-03 17:01:51 +0500

    Checkbox styling

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -11,7 +11,52 @@ const { label, name } = defineProps<Props>();
 
 <template>
   <label>
-    <input type="checkbox" :name="name" v-bind="$attrs" />
+    <input
+      class="visually-hidden"
+      type="checkbox"
+      :name="name"
+      v-bind="$attrs"
+    />
     {{ label }}
   </label>
 </template>
+
+<style scoped>
+label {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  color: var(--text-primary);
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+}
+label::before {
+  display: block;
+  box-sizing: border-box;
+  border: 1px solid rgba(223, 222, 227, 1);
+  border-radius: 4px;
+  width: 20px;
+  height: 20px;
+  content: " ";
+}
+label:has(:checked)::before {
+  border: 1px solid var(--accent);
+  background:
+    center/80% no-repeat url("../assets/check-icon.svg"),
+    var(--accent);
+}
+.visually-hidden {
+  position: absolute;
+  clip-path: inset(100%);
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  width: 1px;
+  height: 1px;
+  white-space: nowrap;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+</style>
