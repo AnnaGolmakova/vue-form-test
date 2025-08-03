f73cf69f935c8923962ef692b42ede02f714ed69
Anna Golmakova <ann.golmakova@gmail.com>
2025-08-03 18:28:06 +0500

    Modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useTemplateRef, defineExpose } from "vue";
+
+const modalRef = useTemplateRef("modal");
+
+function open() {
+  modalRef.value?.showModal();
+}
+
+function close() {
+  modalRef.value?.close();
+}
+
+defineExpose({ open });
+</script>
+
+<template>
+  <dialog ref="modal">
+    <button class="close" @click="close">
+      <span class="visually-hidden">Закрыть</span>
+    </button>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+dialog {
+  border: none;
+  border-radius: 16px;
+  background-color: var(--default-background);
+  padding: 24px;
+  max-width: 516px;
+}
+dialog::backdrop {
+  background-color: var(--overlay);
+}
+.close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  appearance: none;
+  border: none;
+  background: center no-repeat url("../assets/close-icon.svg");
+  width: 24px;
+  height: 24px;
+}
+.visually-hidden {
+  position: absolute;
+  clip-path: inset(100%);
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  width: 1px;
+  height: 1px;
+  white-space: nowrap;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+</style>
